081968a599a4957b10b44c890478484a63100ec8
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-07-22 11:53:07 +0300

    workedVersionGame

--- app/js/main.js
+++ app/js/main.js
@@ -30,36 +30,69 @@ $(function() {
         curElem.toggleClass("accordeon__item-body--hidden")
     });
 
-    //game
+    //game..........................................
     //welcome screen
     $('.rsp-welcome__button').on('click', function(e) {
         $('.rsp-welcome').toggleClass("active")
         $('.rsp-select').toggleClass("active")
     });
 
+    //end screen
+    $('.rsp-end-exit').on('click', function(e) {
+        $('.rsp-end').toggleClass("active")
+        $('.rsp-welcome').toggleClass("active")
+    });
+
+    //play again
+    $('.rsp-end-again').on('click', function(e) {
+        $('.rsp-end').toggleClass("active")
+        $('.rsp-select').toggleClass("active")
+    });
+
     //select screen
     let enemy = ""
-    let enemyImg = ""
+    let enemyImgSc = ""
+    let blok
     $('.rsp-select').on('click', function(e) {
 
         // find name of enemy
         if ($(e.target).hasClass('rsp-select-icon')) {
             enemy = $(e.target).find("p").html()
-            enemyImg = $(e.target).html()
+            blok = $(e.target).html()
+
+            enemyImgSc = $(e.target).find("img").attr("src")
 
         } else if ($(e.target).parents().hasClass('rsp-select-icon')) {
             enemy = $(e.target).parent().find("p").html()
-            enemyImg = $(e.target).parent().html()
+            blok = $(e.target).parent().html()
+
+            enemyImgSc = $(e.target).parent().find("img").attr("src")
+
         }
+        // draw selected enemy name
+        $(".rsp-select-choice").html(enemy)
+
+
+
 
         //start battle if enemy selected
         if ($(e.target).hasClass('rsp-select__button') && enemy) {
 
+            //del old enemyWrap
+            if ($('.enemyIcon')) {
+                console.log("del one")
+                $('.enemyIcon').remove()
+            }
+
             $('.rsp-select').toggleClass("active")
             $('.rsp-game').toggleClass("active")
 
-            //enemyicon
-            $('.rsp-game-enemy').append(enemyImg)
+            //adding enemy icon and name
+            let enemyWrap = document.createElement('div');
+            enemyWrap.className = "enemyIcon";
+            enemyWrap.innerHTML = blok
+            console.log(enemyWrap)
+            $(".rsp-game-enemy").append(enemyWrap)
         }
     });
 
@@ -83,13 +116,14 @@ $(function() {
             return
         }
 
-        alert(playerChoice)
+        // alert(playerChoice)
 
 
     })
 
     //game screen
     let playerChoice = "" // 1 or 2 or 3 
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -98,32 +132,91 @@ $(function() {
     let resultArr = ["YOU WIN", "YOU LOSE", "DRAW"]
     let matrix = new Map
     let roundMessage = ""
-        //1 - dragon 2 - knight 3 - jinny
-        // d win j, k win d, j win k.
-    matrix.set("1 1", result[2]);
-    matrix.set("1 2", result[1]);
-    matrix.set("1 3", result[0]);
-    matrix.set("2 1", result[0]);
-    matrix.set("2 2", result[2]);
-    matrix.set("2 3", result[1]);
-    matrix.set("3 1", result[1]);
-    matrix.set("3 2", result[0]);
-    matrix.set("3 3", result[2]);
+    let playerPoints = 0
+    let enemyPoints = 0
 
-    $('.start!').on('click', function(e) {
-            let computerChoice = getRandomInt(1, 4) // = > 1 || 2 || 3
-            let currentKey = `${playerChoice} ${computerChoice}`
 
-            if (matrix.has(currentKey)) {
-                roundMessage = matrix.get(key)
-                alert(roundMessage)
+    //проверка, кому засчитывать очки
+    function addPoint(key) {
+        if (key == resultArr[0]) {
+            playerPoints += 1
+        } else if (key == resultArr[1]) {
+            enemyPoints += 1
+        } else if (key == resultArr[2]) {
+            return
+        } else { return alert("err addPoint") }
+    }
+
+    // вывод очков
+    function setEnemyPoints(ePoints) {
+        $('.rsp-game-enemyPoints').text(ePoints)
+    }
+
+    function setPlayerPoints(plPoints) {
+        $('.rsp-game-playerPoints').text(plPoints)
+    }
+    setEnemyPoints(enemyPoints)
+    setPlayerPoints(playerPoints)
+
+    function pointsToZero() {
+        enemyPoints = 0
+        playerPoints = 0
+        setEnemyPoints(enemyPoints)
+        setPlayerPoints(playerPoints)
+    }
+
+    //1 - dragon 2 - knight 3 - jinny
+    // d win j, k win d, j win k.
+    matrix.set("1 1", resultArr[2]);
+    matrix.set("1 2", resultArr[1]);
+    matrix.set("1 3", resultArr[0]);
+    matrix.set("2 1", resultArr[0]);
+    matrix.set("2 2", resultArr[2]);
+    matrix.set("2 3", resultArr[1]);
+    matrix.set("3 1", resultArr[1]);
+    matrix.set("3 2", resultArr[0]);
+    matrix.set("3 3", resultArr[2]);
+
+    $('.rsp-game-start').on('click', function(e) {
+        let computerChoice = getRandomInt(1, 4) // = > 1 || 2 || 3
+        let currentKey = `${playerChoice} ${computerChoice}`
+
+        if (matrix.has(currentKey) && playerChoice) {
+            roundMessage = matrix.get(currentKey)
+            alert(roundMessage)
+
+            //добавляем очки
+            addPoint(roundMessage)
+
+            //показываем изменение результата
+            setEnemyPoints(enemyPoints)
+            setPlayerPoints(playerPoints)
+
+            //сбрасываем выбор игрока
+            playerChoice = ""
+
+            //проверяем условия (3 победы)
+            if (playerPoints == 3) {
+                alert("Поздравляем! Победа за вами")
+
+                $('.rsp-game').toggleClass("active")
+                $('.rsp-end').toggleClass("active")
+
+                //сбрасываем счет
+                pointsToZero()
+
+
+            } else if (enemyPoints == 3) {
+                alert("Противник оказался сильнее")
+                $('.rsp-game').toggleClass("active")
+                $('.rsp-end').toggleClass("active")
+
+                //сбрасываем счет
+                pointsToZero()
             }
-        })
-        // изменить счет => прибавить победу победителю
+        }
 
-    // если количество побед 3, сообщаем кто победил и предлагаем сыграть снова или выйти
-    // если выйти => окно приветствия
-    // если сыграть снова => new Game
+    })
 
     // class Game {
     //     // методы класса
